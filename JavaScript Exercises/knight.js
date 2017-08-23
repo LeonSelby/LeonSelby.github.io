0a3d358d5ed10e5f0b8144f07d6aa1d4f61c16c1
Leon Selby <leon_selby@live.co.uk>
2017-08-23 23:10:17 +0100

    BFS att 1, add distance

--- JavaScript Exercises/knight.js
+++ JavaScript Exercises/knight.js
@@ -3,68 +3,73 @@ pointMaker = function (x, y) {
   pt.x = x;
   pt.y = y;
   return pt;
-}
+};
+
+const start = pointMaker(0, 0);
+let count = 0;
+
+const vectorX = [2, 2, -2, -2, 1, 1, -1, -1];
+const vectorY = [-1, 1, 1, -1, 2, -2, 2, -2];
+
+var queue = [];
 
 function tour() {
-  let target = pointMaker(Number(document.getElementById('x').value), Number(document.getElementById('y').value));
-  // let target = pointMaker(2, 8);
-  let count = 0;
+  // let target = pointMaker(Number(document.getElementById('x').value), Number(document.getElementById('y').value));
+  let target = pointMaker(3, 4);
   let tmp = target;
-  console.log(JSON.stringify(tmp));
-  do {
-    if (tmp.x >= 2 && tmp.y >= 1 && tmp.x > tmp.y) { // BOTH POS
-      tmp.x -= 2;
-      tmp.y -= 1;
-      count++;
-      console.log("1st " + count);
-    } else if (tmp.x >= 1 && tmp.y >= 2 && tmp.x < tmp.y) { // BOTH POS
-      tmp.x -= 1;
-      tmp.y -= 2;
-      count++;
-      console.log("2nd " + count);
-      console.log(JSON.stringify(tmp));
+  let i = 1;
+  console.log("input tmp: " + JSON.stringify(tmp));
 
-    } else if (tmp.x <= -1 && tmp.y <= -2 && tmp.x > tmp.y) { // BOTH NEG
-      tmp.x += 1;
-      tmp.y += 2;
-      count++;
-      console.log("3rd " + count);
-    } else if (tmp.x <= -2 && tmp.y <= -1 && tmp.x < tmp.y) { // BOTH NEG
-      tmp.x += 2;
-      tmp.y += 1;
-      count++;
-      console.log("4th " + count);
-    } else if (tmp.x <= -1 && tmp.y >= 2 && tmp.x < tmp.y) { //X is NEG
-      tmp.x += 1;
-      tmp.y -= 2;
-      count++;
-      console.log("5th " + count);
+  populateQueue(tmp);
 
-    } else if (tmp.x <= -2 && tmp.y >= 1 && tmp.x < tmp.y) { //X is NEG
-      tmp.x += 2;
-      tmp.y -= 1;
-      count++;
-      console.log("6th " + count);
-    } else if (tmp.x >= 1 && tmp.y <= -2 && tmp.x > tmp.y) { //Y is NEG
-      tmp.x -= 1;
-      tmp.y += 2;
-      count++;
-      console.log("7th " + count);
-    } else if (tmp.x >= 2 && tmp.y >= -1 && tmp.x > tmp.y) { //Y is NEG
-      tmp.x -= 2;
-      tmp.y += 1;
-      count++;
-      console.log("8th " + count);
-    } else if (tmp.x === tmp.y && tmp.x !== 0) {
-      tmp.x -= 2;
-      tmp.y -= 1;
-      count++;
-      console.log("end " + count);
-    }
-  }
-  while ((tmp.x !== 0) && (tmp.y !== 0));
-  alert("Solution: " + count);
-  console.log("Solution: " + count);
+  queue.forEach((a) => {
+    populateQueue(a).forEach((b) => {
+      populateQueue(b)
+    })
+  })
 }
 
-tour();
+function populateQueue(temp) {
+  count++;
+  let q2 = [];
+  for (let i = 0; i < vectorX.length; i++) {
+    let possX = temp.x + vectorX[i];
+    let possY = temp.y + vectorY[i];
+    let nodeTmp = pointMaker(possX, possY);
+    queue.push(nodeTmp);
+    q2.push(nodeTmp);
+
+    let booltmp = false;
+    for (let entry = 0; entry < queue.length; entry++) {
+      if (queue[entry].x === 0 && queue[entry].y === 0) {
+        booltmp = true;
+      }
+
+      for (let entry = 0; entry < q2.length; entry++) {
+        if (q2[entry].x === 0 && q2[entry].y === 0) {
+          booltmp = true;
+        }
+        if (booltmp === true) {
+          throw '';
+        };
+
+      }
+      console.log("q2: " + JSON.stringify(q2))
+
+      return q2;
+    }
+  }
+}
+// function arrivedBoolean(queueIn) {
+//   let booltmp = false;
+//   for (let entry = 0; entry < queueIn.length; entry++) {
+//     if (queueIn[entry].x === 0 && queueIn[entry].y === 0) {
+//       booltmp = true;
+//       console.log(console.log("arrived in " + count))
+//       throw "";
+//       return booltmp;
+//     }
+//   }
+//   return booltmp;
+// }
+tour()
