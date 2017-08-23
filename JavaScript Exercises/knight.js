9eb452fd6e8508dca0d3750b76262e0a36852218
Leon Selby <leon_selby@live.co.uk>
2017-08-23 23:40:14 +0100

    change to loop

--- JavaScript Exercises/knight.js
+++ JavaScript Exercises/knight.js
@@ -7,69 +7,60 @@ pointMaker = function (x, y) {
 
 const start = pointMaker(0, 0);
 let count = 0;
-
+let finished = false;
 const vectorX = [2, 2, -2, -2, 1, 1, -1, -1];
 const vectorY = [-1, 1, 1, -1, 2, -2, 2, -2];
-
 var queue = [];
+
+function arrivedBoolean(queueIn) {
+  for (let entry = 0; entry < queueIn.length; entry++) {
+    if (queueIn[entry].x === 0 && queueIn[entry].y === 0) {
+      finished = true;
+      return true;
+    }else {
+      return false;
+    }
+  }
+}
+
+function populateQueue(startingPoint) {
+  for (let i = 0; i < vectorX.length; i++) {
+    let possX = startingPoint.x + vectorX[i];
+    let possY = startingPoint.y + vectorY[i];
+    let nodeTmp = pointMaker(possX, possY);
+    queue.push(nodeTmp);
+      if(arrivedBoolean(queue)){
+      break;
+    }
+  }
+}
+
+function popTmpQueue(tempo) {
+  let q2 = [];
+  for (let i = 0; i < vectorX.length; i++) {
+    let possX = tempo.x + vectorX[i];
+    let possY = tempo.y + vectorY[i];
+    let nodeTmp = pointMaker(possX, possY);
+    q2.push(nodeTmp);
+    if(arrivedBoolean(q2)){
+      break;
+    }
+    return q2;
+  }
+}
 
 function tour() {
   // let target = pointMaker(Number(document.getElementById('x').value), Number(document.getElementById('y').value));
   let target = pointMaker(3, 4);
-  let tmp = target;
-  let i = 1;
-  console.log("input tmp: " + JSON.stringify(tmp));
-
-  populateQueue(tmp);
-
-  queue.forEach((a) => {
-    populateQueue(a).forEach((b) => {
-      populateQueue(b)
+  let startingPoint = target;
+  console.log("input tmp: " + JSON.stringify(startingPoint));
+  populateQueue(startingPoint);
+  while (!finished) {
+    queue.forEach((a) => {
+      popTmpQueue(a).forEach((b) => {
+        popTmpQueue(b)
+      })
     })
-  })
-}
-
-function populateQueue(temp) {
-  count++;
-  let q2 = [];
-  for (let i = 0; i < vectorX.length; i++) {
-    let possX = temp.x + vectorX[i];
-    let possY = temp.y + vectorY[i];
-    let nodeTmp = pointMaker(possX, possY);
-    queue.push(nodeTmp);
-    q2.push(nodeTmp);
-
-    let booltmp = false;
-    for (let entry = 0; entry < queue.length; entry++) {
-      if (queue[entry].x === 0 && queue[entry].y === 0) {
-        booltmp = true;
-      }
-
-      for (let entry = 0; entry < q2.length; entry++) {
-        if (q2[entry].x === 0 && q2[entry].y === 0) {
-          booltmp = true;
-        }
-        if (booltmp === true) {
-          throw '';
-        };
-
-      }
-      console.log("q2: " + JSON.stringify(q2))
-
-      return q2;
-    }
   }
 }
-// function arrivedBoolean(queueIn) {
-//   let booltmp = false;
-//   for (let entry = 0; entry < queueIn.length; entry++) {
-//     if (queueIn[entry].x === 0 && queueIn[entry].y === 0) {
-//       booltmp = true;
-//       console.log(console.log("arrived in " + count))
-//       throw "";
-//       return booltmp;
-//     }
-//   }
-//   return booltmp;
-// }
 tour()
