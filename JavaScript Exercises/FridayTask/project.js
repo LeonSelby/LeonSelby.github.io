98e6a77194f72d97954ddd58dc97bcc98a52ef35
Leon Selby <leon_selby@live.co.uk>
2017-08-24 23:36:01 +0100

    templates

---
+++ JavaScript Exercises/FridayTask/project.js
@@ -0,0 +1,28 @@
+
+const bodyEle = document.getElementById("body");
+const formEle = document.getElementById("wrapperDiv");
+const image = document.getElementById("picture");
+const para = document.createElement('p');
+
+
+let counter = 0;
+let example = "please please"
+let example2 = "work"
+
+function test() {
+    let form = document.getElementById("formID");
+    return image != null
+}
+
+console.log(image!=null)
+console.log(formEle!=null)
+console.log(bodyEle!=null)
+
+
+function testAppend(){
+// bodyEle.appendChild(para);
+// formEle.appendChild(para);
+// image.appendChild(para);
+}
+
+testAppend();
