30d45fd45fe073798ac0110ba656984494904ed3
Leon <leon_selby@live.co.uk>
2017-08-25 13:33:53 +0100

    hangman

--- JavaScript Exercises/FridayTask/project.js
+++ JavaScript Exercises/FridayTask/project.js
@@ -1,29 +1,180 @@
 
-const bodyEle = document.getElementById('body')
-const formEle = document.getElementById('wrapperDiv')
-const formEle2 = document.getElementsByName('wrapperDiv')
+const formEle = document.getElementById('wrapperDivID')
 const image = document.getElementById('picture')
-const para = document.createElement('p')
+const paraAbove = document.getElementById('abovePic')
+const livesSection = document.getElementById('belowPic');
+const winSec = document.getElementById('winSection');
+const guessedWrongSection = document.getElementById('guessedWrong');
+const diffPara = document.getElementById('diffPara');
+const entryBox = document.getElementById('entryBoxID')
+const sourcePics = "pics/";
 
-let counter = 0;
-let example = "please please"
-let example2 = "work"
+let lives = 7;
+let numRight = 0;
+let numWrong = 0;
+let chosenWord = "";
+let correctArray = [];
+let guessArray = [];
+let guessString = "";
+let wrongGuesses = [];
 
-function test() {
-    let form = document.getElementById("formID");
-    return image != null
+let easyDifficulty = 1;
+let normalDifficulty = 0;
+let hardDifficulty = 0;
+
+let wordBatch = ["please work", "john", "ben", "leon", "pocketboy", "eastenders", "elliott"]
+
+function setToEasy() {
+    easyDifficulty = 1;
+    normalDifficulty = 0;
+    hardDifficulty = 0;
+    startUp();
+}
+function setToNormal() {
+    easyDifficulty = 0;
+    normalDifficulty = 1;
+    hardDifficulty = 0;
+    startUp();
+}
+function setToHard() {
+    easyDifficulty = 0;
+    normalDifficulty = 0;
+    hardDifficulty = 1;
+    startUp();
 }
 
-console.log(image!=null)
-console.log(formEle!=null)
-console.log(formEle2!=null)
-console.log(bodyEle!=null)
-
-
-function testAppend(){
-// bodyEle.appendChild(para);
-// formEle.appendChild(para);
-// image.appendChild(para);
+function isLetter(a) {
+    return a.toLowerCase() != a.toUpperCase();
 }
 
-testAppend();
+function genWordArray(input) {
+    return input.split('');
+}
+
+function randomNumberGenerator(upto) {
+    return Math.floor(Math.random() * upto);
+}
+
+function chooseRandWord() {
+    let tmp = "";
+    if (easyDifficulty === 1) {
+        do {
+            tmp = wordBatch[randomNumberGenerator(wordBatch.length)]
+            console.log(tmp)
+        } while (tmp.length < 9)
+    }
+    if (normalDifficulty === 1) {
+        do {
+            tmp = wordBatch[randomNumberGenerator(wordBatch.length)]
+        } while (tmp.length > 7 && tmp.length < 5)
+    }
+    if (hardDifficulty === 1) {
+        do {
+            tmp = wordBatch[randomNumberGenerator(wordBatch.length)]
+        } while (tmp.length > 5)
+    }
+    return tmp;
+}
+
+function startUp() {
+    chosenWord = chooseRandWord();
+    correctArray = genWordArray(chosenWord);
+    guessArray = genWordArray(blanksGen(chosenWord));
+    InitialiseLivesSection();
+    updateGuessSection();
+    reset();
+}
+
+function reset() {
+    lives = 7;
+    numRight = 0;
+    numWrong = 0;
+    image.src = sourcePics + numWrong + ".jpg";
+    wrongGuesses = [];
+    winSec.innerHTML = "";
+    guessedWrongSection.innerHTML = "Wrong Guesses:";
+    entryBox.value = "";
+}
+
+function checkVictory() {
+    temp = correctArray.length == guessArray.length
+        && correctArray.every((v, i) => v === guessArray[i]);
+
+    if (temp === true && lives > 0) {
+        winSec.innerHTML = "You win!";
+    }
+    if (temp === false && lives <= 0) {
+        winSec.innerHTML = "You lose!";
+    }
+    if (temp === true && lives <= 0) {
+        winSec.innerHTML = "Took you long enough!"
+    }
+}
+
+function blanksGen(input) {
+    let blankString = "";
+    correctArray.forEach((c) => {
+        if (isLetter(c)) {
+            blankString += "_";
+        } else {
+            blankString += " ";
+        }
+    })
+    return blankString;
+}
+
+function joinArray(input) {
+    return input.join('');
+}
+
+function guess(guessedLetter) {
+    if (!isLetter(guessedLetter)) {
+        window.alert("Input a letter!")
+        return 0;
+    }
+    loseLife = true;
+    for (let i = 0; i < correctArray.length; i++) {
+        if (guessedLetter === correctArray[i]) {
+            guessArray[i] = guessedLetter.valueOf();
+            numRight++;
+            loseLife = false;
+        }
+    }
+    if (loseLife) {
+        wrongGuesses.push(guessedLetter)
+        lives--;
+        numWrong++;
+    }
+    updateGuessedWrong();
+    updateGuessSection();
+    updateImage();
+    checkVictory();
+    entryBox.value = "";
+}
+
+function updateGuessSection() {
+    updateGuessString();
+    paraAbove.innerHTML = guessString;
+}
+
+function InitialiseLivesSection() {
+    livesSection.innerHTML += lives;
+}
+
+function updateGuessString() {
+    guessString = joinArray(guessArray)
+    livesSection.innerHTML = "Lives Left: " + lives;
+}
+
+function updateGuessedWrong() {
+    guessedWrongSection.innerHTML = "Wrong guesses:\n" + wrongGuesses;
+}
+
+function updateImage() {
+    if (lives > -1) {
+        image.src = sourcePics + numWrong + ".jpg";
+    } else {
+        image.src = sourcePics + 7 + ".jpg";
+    }
+
+}
