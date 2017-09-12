e302729664b05014c6ce2590a168b942bcb24cde
LeonSelby <leon_selby@live.co.uk>
2017-09-12 13:28:10 +0100

    Incorporated Async Await, Promises, and now works!

--- JavaScript Exercises/pokemonTask/Untitled-1.js
+++ JavaScript Exercises/pokemonTask/Untitled-1.js
@@ -1,5 +1,35 @@
-const fetch = require('node-fetch')
-const imageLoc = 'https://leonselby.github.io/JavaScript%20Exercises/pokeSprites/';
+// const fetch = require('node-fetch')
+const imageLoc = 'https://leonselby.github.io/JavaScript%20Exercises/pokemonTask/pokeSprites/';
+const typesArray = [
+    "normal",
+    "fighting",
+    "flying",
+    "poison",
+    "ground",
+    "rock",
+    "bug",
+    "ghost",
+    "steel",
+    "fire",
+    "water",
+    "grass",
+    "electric",
+    "psychic",
+    "ice",
+    "dragon",
+    "dark",
+    "fairy"
+]// index + 1 is its ID
+
+Array.prototype.contains = function (obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 
 async function fetchPokemonNames() {
     const response = await fetch(`http://pokeapi.co/api/v2/pokemon/?limit=151`);
@@ -13,7 +43,7 @@ async function fetchPokemonNames() {
 }
 
 async function fetchPokemonMoves(PokeID) {
-    const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${PokeID}`);
+    const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${PokeID + 1}`);
     const data = await response.json();
     let moves = data['moves'];
     moves = moves.map((c) => {
@@ -36,25 +66,89 @@ async function fetchMovesTypes(MoveName) {
     const response = await fetch(`http://pokeapi.co/api/v2/move/${MoveName}`);
     const data = await response.json();
     let type = data['type'].name;
-    type;
     return type;
 }
 
-fetchMovesTypes("razor-wind")
+async function fetchTypeSuperEffectivesPromise(TypeName) {
+    let typeID = typesArray.indexOf(TypeName.toLowerCase()) + 1;
+    const response = await fetch(`http://pokeapi.co/api/v2/type/${typeID}`);
+    const data = await response.json();
+    const damageRelations = data['damage_relations'];
+    const doubleDMGTo = damageRelations['double_damage_to'];
+    let names = doubleDMGTo.map((a) => { return a.name })
+    return names;
+}
+
+async function resolveTypeSupEffectPromise(TypeName) {
+    let names = [];
+    let namesProm = await Promise.resolve(fetchTypeSuperEffectivesPromise(TypeName))
+        .then((a) => {
+            a.forEach((b) => {
+                names.push(b);
+            })
+        })
+    return names;
+}
+
+async function resolveDefTypesPromise(defPokeID) {
+    let defTypes = [];
+    let promise4 = await Promise.resolve(fetchPokemonTypes(defPokeID))
+        .then((z) => {
+            z.forEach((y) => {
+                defTypes.push(y);
+            })
+        })
+    return defTypes;
+}
+
+async function determineResult(moveName, defPokeID) {
+    let counter = 0;
+    let damageMultiplier = 1;
+    let realDefID = defPokeID + 1;
+    let moveType = await resolveMoveTypePromise(moveName);
+    let defTypes = await resolveDefTypesPromise(realDefID); //returns array
+    let doubleAgainstTypes = await resolveTypeSupEffectPromise(moveType); //returns array
+
+    doubleAgainstTypes.forEach((type) => {
+        defTypes.forEach((defType) => {
+            if (type === defType) {
+                counter++;
+            }
+        })
+    })
+    if (counter === 1) {
+        damageMultiplier = 2;
+    } else if (counter === 2) {
+        damageMultiplier = 4;
+    }
+    let outPara = document.getElementById('outputpara');
+    outPara.innerHTML = moveName + " will do " + damageMultiplier + "x damage!";
+
+}
+
+async function resolveMoveTypePromise(moveName) {
+    let moveType;
+    let promise3 = await Promise.resolve(fetchMovesTypes(moveName))
+        .then((a) => moveType = a);
+    return promise3;
+}
 
 function populateNameboxes() {
-    let box = document.getElementsByTagName('select')[0];
-    let box2 = document.getElementsByTagName('select')[1];
-    let names = fetchPokemonNames();
-    names.forEach((a) => {
-        let option = document.createElement('option');
-        let option2 = document.createElement('option');
-        option.text = a;
-        option2.text = a;
-        box2.add(option2, box.length)
-        box.add(option, box.length)
-        option.id = box.length;
-        option2.id = box2.length;
+    let box = document.getElementById('PokemonList1');
+    let box2 = document.getElementById('PokemonList2');
+    let namesArray = [];
+    let promise1 = Promise.resolve(fetchPokemonNames());
+    promise1.then((a) => {
+        a.forEach((b) => {
+            let option = document.createElement('option');
+            let option2 = document.createElement('option');
+            option.text = b;
+            option2.text = b;
+            box2.add(option2, box.length)
+            box.add(option, box.length)
+            option.id = box.length;
+            option2.id = box2.length;
+        })
     })
 }
 
@@ -73,16 +167,16 @@ function populateMovesBox(pokemonID) {
         metabox.removeChild(metabox.childNodes[0]);
     }
     metabox.appendChild(movesBox);
-    let moves = fetchPokemonMoves(pokemonID);
-
-    () => {
-        moves.forEach((b) => {
+    let promise2 = Promise.resolve(fetchPokemonMoves(pokemonID));
+    promise2.then((a) => {
+        a.forEach((b) => {
             let moveOption = document.createElement('option');
             moveOption.text = b;
             movesBox.add(moveOption, movesBox.length);
             moveOption.id = movesBox.length;
         })
-    }
+    })
+
 }
 
 function placeImageAtk(numID) {
@@ -121,3 +215,4 @@ function placeImages(numIDatk, numIDdef) {
     placeImageDef(numIDdef);
 }
 
+populateNameboxes();
